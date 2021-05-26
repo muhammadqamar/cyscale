c9334d4a6fe58bbb8dee40ce1a001e3eb92eac40
Muhammad <muhammadqamar111@gmail.com>
2021-05-26 14:29:24 +0500

    project stablize

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout/layout.js'
+
+const Index = () => (
+  <Layout>
+    <div>
+      <h1 className="text-red-700">i am main</h1>
+      
+    </div>
+  </Layout>
+)
+
+export default Index
